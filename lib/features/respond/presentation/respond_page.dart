aed66f88ca0e052d6e17b028b649d6c1f19901d9
Haz <haziqaiman2@gmail.com>
2026-01-12 18:16:49 +0800

    initial structure

---
+++ lib/features/respond/presentation/respond_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RespondPage extends StatefulWidget {
+  const RespondPage({super.key});
+
+  @override
+  State<RespondPage> createState() => _RespondPageState();
+}
+
+class _RespondPageState extends State<RespondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Respond Page'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:w2b_flutter/features/profile/presentation/profile_page.dart';
+import 'package:w2b_flutter/features/request/presentation/request_page.dart';
+import 'package:w2b_flutter/features/respond/presentation/respond_page.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _views = const [
+    RequestPage(),
+    RespondPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: _views[_currentIndex],
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _currentIndex,
+          onDestinationSelected: _onItemTapped,
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.quiz_outlined), selectedIcon: Icon(Icons.quiz), label: 'Request'),
+            NavigationDestination(icon: Icon(Icons.pin_drop_outlined), selectedIcon: Icon(Icons.pin_drop), label: 'Respond'),
+            NavigationDestination(icon: Icon(Icons.person_outlined), selectedIcon: Icon(Icons.person), label: 'Profile'),
+          ],
         ),
       ),
     );
