d7c1f12ece09592711490a6adb0e5df2bfac0132
Haz <haziqaiman2@gmail.com>
2026-02-09 17:28:00 +0800

    search page sublogic: added placeholder logic to store the text inputted

--- lib/features/search/logic/search_bar_sublogic.dart
+++ lib/features/search/logic/search_bar_sublogic.dart
@@ -6,12 +6,13 @@ class SearchBarSubLogic {
   final SearchPageState state;
   SearchBarSubLogic(this._parent, this.state);
 
+  String _searchText = '';
   /// Prompts the user to log in if not authenticated, else navigates to the new request page.
   void handleNewRequestButtonPressed () {
     if (state.isLoggedIn) {
       // Navigate to request new item page
       ScaffoldMessenger.of(_parent.context).showSnackBar(
-        const SnackBar(content: Text('Navigating to Request New Item Page...')),
+        SnackBar(content: Text('Navigating to Request New Item Page with text: $_searchText...')),
       );
     } else {
       // Prompt user to log in
@@ -31,11 +32,12 @@ class SearchBarSubLogic {
 
   /// Provides search suggestions as the user types.
   void handleSearchInputChanged(String value) {
-    if (value.length < 3) return; // only suggest for 3+ characters
+    _searchText = value;
+    if (_searchText.length < 3) return; // only suggest for 3+ characters
     // TODO: Fetch and display search suggestions
     ScaffoldMessenger.of(_parent.context).showSnackBar(
       SnackBar(
-        content: Text('Display suggestions for: $value'),
+        content: Text('Display suggestions for: $_searchText'),
         //dismiss
         action: SnackBarAction(
           label: 'Dismiss',
@@ -49,7 +51,7 @@ class SearchBarSubLogic {
   void handleSearchSubmitted(String value) {
     // TODO: Execute search with the submitted value
     ScaffoldMessenger.of(_parent.context).showSnackBar(
-      SnackBar(content: Text('Searching for: $value')),
+      SnackBar(content: Text('Searching for: $_searchText')),
     );
   }
 }
