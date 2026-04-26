e87889c5528c29a3369d7fa6eb3074f471f1d317
Haz <haziqaiman2@gmail.com>
2026-04-26 17:28:39 +0800

    SearchPage updates: - added Chip to display the number of search results - added animated dismissable Chip to lock the search bar - pass callbacks that previous commits missed

--- lib/features/search/presentation/search_page.dart
+++ lib/features/search/presentation/search_page.dart
@@ -21,12 +21,36 @@ class SearchPage extends StatefulWidget {
   State<SearchPage> createState() => _SearchPageState();
 }
 
+class _SearchPageState extends BaseState<SearchPage, SearchPageController, SearchPageUiEvent> with SingleTickerProviderStateMixin{
   final TextEditingController searchBarController = TextEditingController();
   
   @override
   SearchPageController initController() => SearchPageController(widget.dio, searchBarController);
 
   double get mapWidth => MediaQuery.of(context).size.width - 16;
+
+  late AnimationController _animationController;
+  late Animation<double> _shakeAnimation;
+  late Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
+
+    _shakeAnimation = TweenSequence([
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: 5.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: 5.0, end: -5.0), weight: 2),
+      TweenSequenceItem(tween: Tween(begin: -5.0, end: 5.0), weight: 2),
+      TweenSequenceItem(tween: Tween(begin: 5.0, end: 0.0), weight: 1),
+    ]).animate(_animationController);
+
+    _colorAnimation = TweenSequence<Color?>([
+      TweenSequenceItem(tween: ColorTween(begin: Colors.white, end: Colors.redAccent), weight: 1),
+      TweenSequenceItem(tween: ColorTween(begin: Colors.redAccent, end: Colors.white), weight: 1),
+    ]).animate(_animationController);
+  }
 
   @override
   void handleUIEvent(SearchPageUiEvent event) {
@@ -82,6 +106,7 @@ class SearchPage extends StatefulWidget {
           Navigator.of(context).pop();
           break;
         case SearchPageUiEvent.searchLockedWarning:
+          _animationController.forward(from: 0.0);
           break;
     }
   }
@@ -92,9 +117,9 @@ class SearchPage extends StatefulWidget {
       isScrollControlled: true,
       builder: (context) => NewInquiryForm(
         itemName: controller.searchBarSubLogic.searchText,
-        description: controller.searchBarSubLogic.description,
+        // description: controller.searchBarSubLogic.description,
         onItemNameChanged: (value) => controller.searchBarSubLogic.searchText = value,
-        onDescriptionChanged: (value) => controller.searchBarSubLogic.description = value,
+        onDescriptionChanged: (value) {},
         onSubmit: () => controller.searchBarSubLogic.handleSendNewRequest(),
 
         listenable: controller,
@@ -111,20 +136,52 @@ class SearchPage extends StatefulWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          BaseSearchBar(
-            useSearchViewForSuggestions: true,
-            controller: controller,
-            hintText: 'Search for items...',
-            trailing: [
-              IconButton(
-                onPressed: controller.searchBarSubLogic.handleNewRequestButtonPressed, 
-                icon: const Icon(Icons.post_add), 
-                tooltip: "Request new item",
-              ),
-            ],
-            suggestions: controller.searchBarSubLogic.searchSuggestions,
-            onChanged: (value) async => await controller.searchBarSubLogic.handleSearchInputChanged(value),
-            onSubmitted: (value) => controller.searchBarSubLogic.handleSearchSubmitted(value),
+          ListenableBuilder(
+            listenable: controller,
+            builder: (context, _) => Stack(
+              alignment: Alignment.centerLeft,
+              children: [
+                BaseSearchBar(
+                  onTap: controller.searchBarSubLogic.handleSearchBarTapped,
+                  searchController: searchBarController,
+                  useSearchViewForSuggestions: true,
+                  listenable: controller,
+                  hintText: controller.state.hasSelectedSearchResult ? '' :  'Search for items...',
+                  trailing: [
+                    IconButton(
+                      onPressed: controller.searchBarSubLogic.handleNewRequestButtonPressed, 
+                      icon: const Icon(Icons.post_add), 
+                      tooltip: "Request new item",
+                    ),
+                  ],
+                  suggestions: controller.searchBarSubLogic.searchSuggestions,
+                  onChanged: (value) async => await controller.searchBarSubLogic.handleSearchInputChanged(value),
+                  onSubmitted: (value) => controller.searchBarSubLogic.handleSearchSubmitted(value),
+                  onSuggestionSelected: (index) => controller.searchBarSubLogic.handleSearchSuggestionSelected(index),
+                ),
+                // Selected search result chip
+                // TODO: confirm if chip works as intended when the search result name is long, maybe set a max width and use ellipsis if it exceeds that
+                if (controller.state.hasSelectedSearchResult)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 64.0),
+                    child: AnimatedBuilder(
+                      animation: _animationController,
+                      builder: (context, _) => Transform.translate(
+                        offset: Offset(_shakeAnimation.value, 0),
+                        child: Chip(
+                          backgroundColor: _colorAnimation.value,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          label: Text(controller.searchBarSubLogic.selectedSuggestion!.modelName),
+                        deleteIcon: const Icon(Icons.close),
+                        onDeleted: controller.searchBarSubLogic.handleClearSelectedSuggestion,
+                      ),
+                    ),
+                  ),
+                ),
+              ]
+            ),
           ),
           // Google Map
           Expanded(
@@ -132,30 +189,43 @@ class SearchPage extends StatefulWidget {
               padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
               child: ListenableBuilder(
                 listenable: controller,
-                builder: (context, _) => MapWidget(
-                  showMyLocationButton: true,
-                  showZoomControls: true,
-                  extraButtons: [
-                    MapSecondaryButton(
-                      icon: Icon(controller.state.lockSearchArea ? Icons.lock : Icons.lock_open),
-                      onPressed: controller.secondaryButtonsSubLogic.handleSearchAreaLockToggle,
-                      tooltip: controller.state.lockSearchArea ? 'Unlock Search Area' : 'Lock Search Area',
+                builder: (context, _) => Stack(
+                  children: [
+                      MapWidget(
+                      showMyLocationButton: true,
+                      showZoomControls: true,
+                      extraButtons: [
+                        MapSecondaryButton(
+                          icon: Icon(controller.state.lockSearchArea ? Icons.lock : Icons.lock_open),
+                          onPressed: controller.secondaryButtonsSubLogic.handleSearchAreaLockToggle,
+                          tooltip: controller.state.lockSearchArea ? 'Unlock Search Area' : 'Lock Search Area',
+                        ),
+                        MapSecondaryButton(
+                          onPressed: !controller.state.lockSearchArea ? null : controller.secondaryButtonsSubLogic.handleMoveSearchAreaToCameraButtonPressed,
+                          icon: const Icon(Icons.pin_drop_outlined),
+                          tooltip: 'Move search area to current location',
+                        ),
+                        MapSecondaryButton(
+                          onPressed: !controller.state.lockSearchArea ? null : () => controller.moveCameraToSearchLocation(mapWidth, animate: true),
+                          icon: const Icon(Icons.center_focus_strong_outlined),
+                          tooltip: 'Move Map to Search Area',  
+                        ),
+                      ],
+                      onLocationInitialized: (position) => controller.mapSubLogic.handleOnLocationInitialized(position, mapWidth),
+                      onMapCreated: controller.mapSubLogic.handleMapCreated,
+                      onCameraMove: controller.mapSubLogic.handleCameraMove,
+                      onCameraIdle: controller.mapSubLogic.handleCameraIdle,
+                      circles: {controller.searchRangeCircle},
+                      markers: controller.state.markers.toSet(),
                     ),
-                    MapSecondaryButton(
-                      onPressed: !controller.state.lockSearchArea ? null : controller.secondaryButtonsSubLogic.handleMoveSearchAreaToCameraButtonPressed,
-                      icon: const Icon(Icons.pin_drop_outlined),
-                      tooltip: 'Move search area to current location',
-                    ),
-                    MapSecondaryButton(
-                      onPressed: !controller.state.lockSearchArea ? null : () => controller.moveCameraToSearchLocation(mapWidth, animate: true),
-                      icon: const Icon(Icons.center_focus_strong_outlined),
-                      tooltip: 'Move Map to Search Area',  
-                    ),
-                  ],
-                  onLocationInitialized: (position) => controller.mapSubLogic.handleOnLocationInitialized(position, mapWidth),
-                  onMapCreated: controller.mapSubLogic.handleMapCreated,
-                  onCameraMove: controller.mapSubLogic.handleCameraMove,
-                  circles: {controller.searchRangeCircle},
+                    if (controller.state.hasSelectedSearchResult)
+                      Positioned(
+                        child: Chip(
+                          backgroundColor: _colorAnimation.value,
+                          label: Text("Search result: ${controller.searchBarSubLogic.isSearchingForAnswers ? 'Searching...' : '${controller.state.markers.length} found'}" ),
+                        )
+                      )
+                  ]
                 ),
               ),
             ),
