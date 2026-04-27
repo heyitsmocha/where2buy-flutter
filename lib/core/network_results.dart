4bb0b532cd68753d1632e7d5f172f9816375a94f
Haz <haziqaiman2@gmail.com>
2026-04-27 19:06:32 +0800

    Result: added comments

--- lib/core/network_results.dart
+++ lib/core/network_results.dart
@@ -8,12 +8,14 @@ sealed class Result<T> {
   factory Result.error(String errorMessage, {int? statusCode}) = Failure;
 }
 
+/// Represents a successful result containing a value of type T as [value].
 class Success<T> extends Result<T> {
   final T value;
 
   const Success(this.value);
 }
 
+/// Represents a failed result containing an error message and an optional status code. with [errorMessage] and [statusCode].
 class Failure<T> extends Result<T> {
   final String errorMessage;
   final int? statusCode;
