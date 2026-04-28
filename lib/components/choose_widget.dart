7d8d96a7e309b542672b3344423c8cf96185769b
Haz <haziqaiman2@gmail.com>
2026-04-28 15:45:41 +0800

    Choose: pass WidgetBuilder instead of Widget

--- lib/components/choose_widget.dart
+++ lib/components/choose_widget.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
+/// A simple widget that chooses between two widgets to display based on a boolean condition. This is useful for showing loading states, empty states, or toggling between different views without having to write if-else logic directly in the build method of the parent widget.<br><br>
+/// Usage: <br>
+/// ```dart
+/// Choose(
+///   condition: isLoading,
+///   ifTrue: (context) => CircularProgressIndicator(),
+///   ifFalse: (context) => Text('Data Loaded'),
+/// );
+/// ```
 class Choose extends StatelessWidget {
-  final Widget ifTrue;
-  final Widget ifFalse;
+  // Use WidgetBuilder instead of direct Widget to ensure the latest value is always used when the widget is built, especially if the condition changes and triggers a rebuild
+  /// The widget to display when the condition is true
+  final WidgetBuilder ifTrue;
+  /// The widget to display when the condition is false
+  final WidgetBuilder ifFalse;
+  /// The condition to evaluate to determine which widget to display
   final bool condition;
   
   const Choose({
@@ -14,6 +27,6 @@ class Choose extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return condition ? ifTrue : ifFalse;
+    return condition ? ifTrue(context) : ifFalse(context);
   }
 }
