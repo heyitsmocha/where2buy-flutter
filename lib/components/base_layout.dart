45c673ec93fce18c18fccdd5155df4acd4d41cbf
Haz <haziqaiman2@gmail.com>
2026-04-29 17:59:37 +0800

    BaseLayout: added flags to enable/disable directional paddings

--- lib/components/base_layout.dart
+++ lib/components/base_layout.dart
@@ -2,15 +2,22 @@ import 'package:flutter/material.dart';
 
 class BaseLayout extends StatelessWidget {
   final Widget child;
+  final bool topPadding, bottomPadding, leftPadding, rightPadding;
 
   /// A simple base layout that centers its child and adds padding of 8.0 on all sides.
-  const BaseLayout({super.key, required this.child});
+  /// The top, bottom, left, and right padding can be optionally disabled by setting [topPadding], [bottomPadding], [leftPadding], and [rightPadding] to false.
+  const BaseLayout({super.key, required this.child, this.topPadding = true, this.bottomPadding = true, this.leftPadding = true, this.rightPadding = true});
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: EdgeInsets.only(
+          top: topPadding ? 8.0 : 0.0,
+          bottom: bottomPadding ? 8.0 : 0.0,
+          left: leftPadding ? 8.0 : 0.0,
+          right: rightPadding ? 8.0 : 0.0,
+        ),
         child: child,
       ),
     );
