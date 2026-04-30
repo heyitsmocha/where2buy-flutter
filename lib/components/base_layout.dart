825b6e5622cd0d4a9c1c3e6821c9309826f0c143
Haz <haziqaiman2@gmail.com>
2026-04-30 13:24:41 +0800

    BaseLayout: removed Center widget

--- lib/components/base_layout.dart
+++ lib/components/base_layout.dart
@@ -4,22 +4,20 @@ class BaseLayout extends StatelessWidget {
   final Widget child;
   final bool topPadding, bottomPadding, leftPadding, rightPadding;
 
-  /// A simple base layout that centers its child and adds padding of 8.0 on all sides.
-  /// The top, bottom, left, and right padding can be optionally disabled by setting [topPadding], [bottomPadding], [leftPadding], and [rightPadding] to false.
+  /// A simple base layout that adds padding of 8.0 on all sides.
+  /// The individual paddings can be optionally disabled by setting [topPadding], [bottomPadding], [leftPadding], and [rightPadding] to false.
   const BaseLayout({super.key, required this.child, this.topPadding = true, this.bottomPadding = true, this.leftPadding = true, this.rightPadding = true});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.only(
-          top: topPadding ? 8.0 : 0.0,
-          bottom: bottomPadding ? 8.0 : 0.0,
-          left: leftPadding ? 8.0 : 0.0,
-          right: rightPadding ? 8.0 : 0.0,
-        ),
-        child: child,
+    return Padding(
+      padding: EdgeInsets.only(
+        top: topPadding ? 8.0 : 0.0,
+        bottom: bottomPadding ? 8.0 : 0.0,
+        left: leftPadding ? 8.0 : 0.0,
+        right: rightPadding ? 8.0 : 0.0,
       ),
+      child: child,
     );
   }
 }
